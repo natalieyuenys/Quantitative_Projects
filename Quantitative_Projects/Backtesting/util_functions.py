41dcca3918eedb82e558a1c145d6486b70c88aec
natalieyuenys <60304854+natalieyuenys@users.noreply.github.com>
2024-03-15 12:23:00 +0800

    Add files via upload

---
+++ Quantitative_Projects/Backtesting/util_functions.py
@@ -0,0 +1,60 @@
+import pandas as pd
+
+import yfinance as yf
+
+import datetime
+from dateutil.relativedelta import relativedelta
+
+
+def getdata(sym, close):
+    
+    today = datetime.datetime.now()
+    df = yf.download(sym, start=datetime.datetime(today.year-2,today.month, 1),
+                                    end=datetime.datetime(today.year, today.month, 1)) 
+    pd.set_option('display.max_columns', None)
+
+    df = df[['Open','High','Low',close,'Volume']]
+    df = df.reset_index().rename(columns={'index':'Date'})
+    df['ticker'] =sym
+
+    return df
+
+def get_backtest_data(df, n_months):
+
+    today = datetime.datetime.now()
+    current_month = datetime.datetime(today.year,today.month, 1)
+    past_n_months = current_month-relativedelta(months=n_months)
+
+    df = df[(df['Date']>=past_n_months)&(df['Date']<current_month)]
+
+    print("Backtesting on previous {} months".format(n_months))
+
+    return df
+
+def update_position(position, signal):
+
+    if position!=0:
+        return signal
+    else:
+        return position
+    
+def get_df_trade_position(df,col_position, col_signal):
+    
+    df[col_position] = df[col_signal].diff()
+    
+    df[col_position] = df.apply(lambda row: uf.update_position(row[col_position],row[col_signal]), axis=1)
+    
+    return df
+
+def get_df_trade_return(df, col_position):
+
+    df = df[df[col_position]!=0]
+    
+    if df.loc[:,'Position'].iloc[0] == -1:
+        # Remove the first row
+        df = df.iloc[1:, :]
+    
+    df['return'] = df['Previous_day_close'].diff()
+    df['rate of return'] = df['Previous_day_close'].pct_change()
+
+    return df[df[col_position]==-1]
---
+++ Quantitative_Projects/Stock_Analysis/Pair_Trading/README.md
@@ -0,0 +1,3 @@
+# Pair Trading
+
+This repo is for finding stocks with high correlations or lagged correlations for pair-trading opportunities.
