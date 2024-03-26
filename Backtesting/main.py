6b6cb8ff5eb12c0c63940d3ed0f9030d133b275e
natalieyuenys <natalie.yuen.ys@gmail.com>
2024-03-26 11:17:39 +0800

    Feat: Automation

--- Backtesting/main.py
+++ Backtesting/main.py
@@ -4,7 +4,10 @@ warnings.filterwarnings("ignore")
 import pandas as pd
 
 import utils.util_functions as uf
-import strategies as s
+from strategies import sma_crossover
+from strategies import rsi_crossover
+from strategies import rsi_range
+from strategies import support_resistance_breakout
 import utils.backtesting as bt
 import utils.visualisation as vs
 
@@ -14,11 +17,11 @@ list_stock = ['AAPL','TSLA','NVDA','GOOG','META','ADBE','AMZN','MSFT','KO']
 
 risk_free_rate = 2.24
 
-short_sma = 10
-long_sma = 200
+list_short_sma = [50]
+list_long_sma = [100,150,200]
 
-short_rsi = 7
-long_rsi = 14
+list_short_rsi = [6,7]
+list_long_rsi = [13,14]
 
 rsi_range_period = 14
 
@@ -26,56 +29,70 @@ list_strategy = ['sma_crossover','rsi_range','rsi_crossover','support_resistance
 
 for strategy in list_strategy:
 
-# Create an empty dataframe with column names
-    df_analysis_heatmap = pd.DataFrame(
-        columns= [
-            'Stock',
-            'No. of trades', 
-            'Trade Return', 
-            'Avg Return (%)', 
-            'Best Trade (%)', 
-            'Worst Trade (%)',
-            'Win Rate (%)',
-            'Win-Loss Ratio',
-            'Sharpe Ratio'
-            ]
-        )
-
     df_hyp_test = pd.DataFrame()
+    
+    ######### Execute Strategies                            
+    if strategy == 'sma_crossover':
+        for short_sma in list_short_sma:
+            for long_sma in list_long_sma: 
+                df_analysis_heatmap = vs.gen_heatmap_df()    
+                for ticker in list_stock :
+                    try:
+                        df_raw = uf.getdata(ticker, close = "Adj Close")   
+                        df = sma_crossover.get_signal(df_raw, close='Adj Close', sma_value=[short_sma,long_sma], stop_loss_pct=0.03)
+                        df_trades, df_heatmap = bt.df_backtesting(df, ticker, risk_free_rate)
+                        title = 'Performance Metrics for sma_crossover with parameter ({},{})'.format(short_sma, long_sma)
+                    except Exception as e:
+                        print(f"Error occurred: {e}")
+                        continue
+                    df_analysis_heatmap = pd.concat([df_analysis_heatmap, df_heatmap])
+                vs.gen_analysis_heatmap(df_analysis_heatmap, title)
 
-    for ticker in list_stock :
-
-        try:
-            df_raw = uf.getdata(ticker, close = "Adj Close")
-            
-            ######### Execute Strategies                            
-            if strategy == 'sma_crossover':
-                df = s.signal_sma_crossover(df_raw, close='Adj Close', sma_value=[short_sma,long_sma], stop_loss_pct=0.03)
-                df_trades, df_heatmap = bt.df_backtesting(df, ticker, risk_free_rate)
-                title = 'Performance Metrics for sma_crossover with parameter ({},{})'.format(short_sma, long_sma)
-
-            elif strategy == 'rsi_range':
-                df = s.signal_rsi_range(df_raw, n_periods=rsi_range_period, close='Adj Close',overbought_threshold=70, oversold_threshold=30, stop_loss_pct=0.03)
+    elif strategy == 'rsi_range':
+        df_analysis_heatmap = vs.gen_heatmap_df()
+        for ticker in list_stock :
+            try:
+                df_raw = uf.getdata(ticker, close = "Adj Close")   
+                df = rsi_range.get_signal(df_raw, n_periods=rsi_range_period, close='Adj Close',overbought_threshold=70, oversold_threshold=30, stop_loss_pct=0.03)
                 df_trades, df_heatmap = bt.df_backtesting(df, ticker, risk_free_rate)
                 title = 'Performance Metrics for rsi_range with parameter {}'.format(rsi_range_period)
+            except Exception as e:
+                print(f"Error occurred: {e}")
+                continue
+            df_analysis_heatmap = pd.concat([df_analysis_heatmap, df_heatmap])
+        vs.gen_analysis_heatmap(df_analysis_heatmap, title)
 
-            elif strategy == 'rsi_crossover':            
-                df = s.signal_rsi_crossover(df_raw, close="Adj Close", list_periods=[short_rsi,long_rsi], stop_loss_pct=0.03)
-                df_trades, df_heatmap =bt.df_backtesting(df, ticker, risk_free_rate)
-                title = 'Performance Metrics for rsi_crossover with parameter ({},{})'.format(short_rsi, long_rsi)
+    elif strategy == 'rsi_crossover':            
+        for short_rsi in list_short_rsi:
+            for long_rsi in list_long_rsi:  
+                df_analysis_heatmap = vs.gen_heatmap_df()
+                for ticker in list_stock :
+                    try:
+                        df_raw = uf.getdata(ticker, close = "Adj Close")     
+                        df = rsi_crossover.get_signal(df_raw, close="Adj Close", list_periods=[short_rsi,long_rsi], stop_loss_pct=0.03)
+                        df_trades, df_heatmap =bt.df_backtesting(df, ticker, risk_free_rate)
+                        title = 'Performance Metrics for rsi_crossover with parameter ({},{})'.format(short_rsi, long_rsi)
+                    except Exception as e:
+                        print(f"Error occurred: {e}")
+                        continue
+                    df_analysis_heatmap = pd.concat([df_analysis_heatmap, df_heatmap])
+            vs.gen_analysis_heatmap(df_analysis_heatmap, title)
 
-            elif strategy =='support_resistance_breakout':
-                df = s.signal_breakout(df_raw, 'Adj Close', 60, 6, 10)
+    elif strategy =='support_resistance_breakout':
+        df_analysis_heatmap = vs.gen_heatmap_df()
+        for ticker in list_stock :
+            try:
+                df_raw = uf.getdata(ticker, close = "Adj Close")  
+                df = support_resistance_breakout.get_signal(df_raw, 'Adj Close', 60, 6, 10)
                 df_trades, df_heatmap =bt.df_backtesting(df, ticker, risk_free_rate)
                 title = 'Performance Metrics for support_resistance_breakout'
-            
-            df_test = pd.DataFrame({ticker:df_trades['rate of return']})
+            except Exception as e:
+                print(f"Error occurred: {e}")
+                continue
+            df_analysis_heatmap = pd.concat([df_analysis_heatmap, df_heatmap])
+        vs.gen_analysis_heatmap(df_analysis_heatmap, title)
+            # df_test = pd.DataFrame({ticker:df_trades['rate of return']})
 
-        except Exception as e:
-            print(f"Error occurred: {e}")
-            continue
-
-        df_analysis_heatmap = pd.concat([df_analysis_heatmap, df_heatmap])
-        df_hyp_test = pd.concat([df_hyp_test, df_test.reset_index(drop=True)], axis=1)
+        # df_hyp_test = pd.concat([df_hyp_test, df_test.reset_index(drop=True)], axis=1)
         
-    vs.gen_analysis_heatmap(df_analysis_heatmap, title)
+    
