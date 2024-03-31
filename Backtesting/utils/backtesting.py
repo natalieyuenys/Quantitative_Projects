bf355eeeccf81b659b3a89204b6a6776e2868af1
natalieyuenys <natalie.yuen.ys@gmail.com>
2024-03-31 23:20:15 +0800

    Feat: add ROI

--- Backtesting/utils/backtesting.py
+++ Backtesting/utils/backtesting.py
@@ -3,20 +3,19 @@ import pandas as pd
 import utils.util_functions as uf
 import utils.performance_metrics as pm
 
-def df_backtesting(df, ticker, risk_free_rate):
+def df_backtesting(df, ticker, investment, risk_free_rate):
 
     global df_trades
 
     df['Previous_day_close'] = df['Adj Close'].shift(1)
 
-    df_backtest = uf.get_backtest_data(df, 12)
-
-    df_position = uf.get_df_trade_position(df_backtest, 'Position', 'Signal')
+    df_position = uf.get_df_trade_position(df, 'Position', 'Signal')
 
     df_trades = uf.get_df_trade_return(df_position, 'Position')
 
     n_trades = pm.get_number_trades(ticker, df_trades)
     trade_return = pm.get_trade_return(ticker, df_trades)
+    roi =  pm.get_return_on_investment(df_trades, investment)
     avg_return = pm.get_avg_rate_return(ticker, df_trades)
     best_trade = pm.get_best_trade_return(ticker, df_trades)
     worst_trade = pm.get_worst_trade_return(ticker, df_trades)
@@ -27,7 +26,8 @@ def df_backtesting(df, ticker, risk_free_rate):
     df_heatmap = pd.DataFrame({
         'Stock':[ticker],
         'No. of trades': [n_trades], 
-        'Trade Return': [trade_return], 
+        'Trade Return': [trade_return],
+        'ROI': [roi],
         'Avg Return (%)': [avg_return],
         'Best Trade (%)':[best_trade],
         'Worst Trade (%)':[worst_trade],
